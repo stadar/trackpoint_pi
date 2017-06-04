86e8d9c173633f80d71250515a296c9c2b62f016
drid <drid@stadar.org>
2017-06-04 03:05:01 +0300

    Changed TTM bearing from Relative to True

--- src/trackpoint_pi.cpp
+++ src/trackpoint_pi.cpp
@@ -180,7 +180,7 @@ wxString trackpoint_pi::PORE_2_TTM(NMEA0183 nmea, wxString name)
     double distance;
 
     distance = sqrt(pow(nmea.Pore.X, 2) + pow(nmea.Pore.Y, 2))/1000;
-    TTM += wxString::Format(wxT("$TPTTM,%02d,%1.4f,%3.1f,R,,,,,,K,%s,T,,%s,A*"),
+    TTM += wxString::Format(wxT("$TPTTM,%02d,%1.4f,%3.1f,T,,,,,,K,%s,T,,%s,A*"),
                             nmea.Pore.TargetNumber,
                             distance,
                             nmea.Pore.TargetBearing,
